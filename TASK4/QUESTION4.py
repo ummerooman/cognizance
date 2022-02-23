5bb7e79505c595b2b42f09070bd23d8b63d2f649
ummerooman <99654067+ummerooman@users.noreply.github.com>
2022-02-23 19:25:46 +0530

    Create QUESTION4.py

---
+++ TASK4/QUESTION4.py
@@ -0,0 +1,11 @@
+n = int(input("enter the number"))
+temp = n
+rev = 0
+while (n>0):
+    digit = n%10
+    rev = rev*10 + digit
+    n = n//10
+if (temp == rev):
+    print ("the number is a palindrome")
+else:
+    print ("the number is not a palindrome")
