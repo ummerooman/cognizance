7cd9a4199d4d1d6f5cafe5cc6e6778c088b38fc6
ummerooman <99654067+ummerooman@users.noreply.github.com>
2022-03-09 22:42:34 +0530

    Create Q5iv.py

---
+++ TASK-8/Q5iv.py
@@ -0,0 +1,9 @@
+import numpy
+import random
+x = numpy.array([1.0, 2.0])
+print("\nFloat Datatype:")
+print(x.dtype)
+
+x= numpy.array([1, 2], dtype = numpy.int64)
+print("\nchanging a datatype:")
+print(x.dtype)
